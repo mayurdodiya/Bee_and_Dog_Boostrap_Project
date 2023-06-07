e8b08f8344bcb3530b8558564dd853212cbf7538
mayurdodiya <132254613+mayurdodiya@users.noreply.github.com>
2023-06-07 16:47:54 +0530

    Mayur dodiya

---
+++ scamm.py
@@ -0,0 +1,9 @@
+import "f:\Codding\Boostrap\my learning\Boostrao Project2\scamm.py"
+import time
+time.sleep(4)
+count=0
+while count<=1000:
+    pyautogui.typewrite("i love you"+str(count))
+    pyautogui.press("enter")
+    count=count+1
+    
